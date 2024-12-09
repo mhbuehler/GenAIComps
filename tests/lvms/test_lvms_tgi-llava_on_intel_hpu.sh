51ef61fed5d2495711ae741b98f44d3364a757f3
dmsuehir <dina.s.jones@intel.com>
2024-12-09 22:27:27 +0000

    Test code update
    Signed-off-by: dmsuehir <dina.s.jones@intel.com>

--- tests/lvms/test_lvms_tgi-llava_on_intel_hpu.sh
+++ tests/lvms/test_lvms_tgi-llava_on_intel_hpu.sh
@@ -24,14 +24,14 @@ function build_docker_images() {
 function start_service() {
     unset http_proxy
     model="llava-hf/llava-v1.6-mistral-7b-hf"
-    lvm_port=9399 #5050
+    lvm_port=5050
     docker run -d --name="test-comps-lvm-tgi-llava" -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 5027:80 --runtime=habana -e PT_HPU_ENABLE_LAZY_COLLECTIVES=true -e SKIP_TOKENIZER_IN_TGI=true -e HABANA_VISIBLE_DEVICES=all  -e OMPI_MCA_btl_vader_single_copy_mechanism=none --cap-add=sys_nice --ipc=host ghcr.io/huggingface/tgi-gaudi:2.0.5 --model-id $model --max-input-tokens 4096 --max-total-tokens 8192
     docker run -d --name="test-comps-lvm-tgi" -e LVM_ENDPOINT=http://$ip_address:5027 -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p $lvm_port:9399 --ipc=host opea/lvm-tgi:comps
     sleep 3m
 }
 
 function validate_microservice() {
-    lvm_port=9399 #5050
+    lvm_port=5050
     result=$(http_proxy="" curl http://localhost:$lvm_port/v1/lvm -XPOST -d '{"image": "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFUlEQVR42mP8/5+hnoEIwDiqkL4KAcT9GO0U4BxoAAAAAElFTkSuQmCC", "prompt":"What is this?"}' -H 'Content-Type: application/json')
     if [[ $result == *"yellow"* ]]; then
         echo "LVM prompt with an image - Result correct."
@@ -98,15 +98,15 @@ function stop_docker() {
 
 function main() {
 
-    #stop_docker
+    stop_docker
 
-    #build_docker_images
-    #start_service
+    build_docker_images
+    start_service
 
     validate_microservice
 
-    #stop_docker
-    #echo y | docker system prune
+    stop_docker
+    echo y | docker system prune
 
 }
 
